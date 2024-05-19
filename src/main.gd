f74a1d3e30b6d5a87de616d2d6d76aed66a936e7
Bjørnar Prytz <bjornarprytz@gmail.com>
2024-05-19 22:55:00 +0200

    points tweak

--- src/main.gd
+++ src/main.gd
@@ -14,18 +14,17 @@ var food_interval = 10
 
 func _process(delta):
 
-	interval -= delta
+    interval -= delta
 
-	if interval <= 0:
-		interval = 0.1
-		snake.move()
-		food_interval -= 1
-		if food_interval <= 0:
-			food_interval = 10
-			map.spawn_food()
+    if interval <= 0:
+        interval = 0.1
+        snake.move()
+        food_interval -= 1
+        if food_interval <= 0:
+            food_interval = 10
+            map.spawn_food()
 
-
-func _on_snake_points(value: int) -> void:
-	points += value
-	
-	score.text = str(points)
+func _on_snake_snip(value: int) -> void:
+    points += (value * value)
+    
+    score.text = str(points)
